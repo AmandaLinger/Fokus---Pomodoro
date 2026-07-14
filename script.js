07553054392a53be64b438c4566fbe5f31ca88d6
Amanda Marques <amandamarqueslinger@gmail.com>
2026-07-14 15:14:08 -0300

    implementando classList

--- script.js
+++ script.js
@@ -14,21 +14,54 @@ const duracaoFoco = 1500;
 const duracaoDescansoCurto = 300; 
 const duracaoDescansoLongo = 900; 
 
+const botoes = document.querySelectorAll('.app__card-button')
 
 focoBtn.addEventListener('click', () => {
     alterarContexto('foco');
+    focoBtn.classList.add('active')
 });
 
 curtoBtn.addEventListener('click', () => {
     alterarContexto('descanso-curto');
+    curtoBtn.classList.add('active')
 });
 
 longoBtn.addEventListener('click', () => {
     alterarContexto('descanso-longo');
-
+    longoBtn.classList.add('active')
 });
 
 function alterarContexto(contexto){
+    botoes.forEach( function (contexto) {
+        contexto.classList.remove('active')
+    })
+
+
     html.setAttribute('data-contexto', contexto);
     tipoImagem.setAttribute('src', `/imagens/${contexto}.png`);
+
+    switch (contexto){
+        case "foco":
+            fraseTitle.innerHTML= `
+            Otimize sua produtividade,<br>
+                <strong class="app__title-strong">mergulhe no que importa.</strong>
+            `
+        break;
+
+        case "descanso-curto":
+        fraseTitle.innerHTML=`
+        Que tal dar uma respirada? <strong class="app__title-strong"> Faça uma pausa curta!</strong>
+        `
+        break;
+
+        case 'descanso-longo':
+            fraseTitle.innerHTML=`
+            Hora de voltar à superfície. <strong class="app__title-strong"> Faça uma pausa longa.</strong>
+            `
+        break;
+
+        default:
+            break;
+
+    }
 }
