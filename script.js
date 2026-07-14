64057140ba267942aa29c78c3df434fdde7993e5
Amanda Marques <amandamarqueslinger@gmail.com>
2026-07-14 10:04:11 -0300

    separando funções e métodos

--- script.js
+++ script.js
@@ -1,16 +1,34 @@
 const html = document.querySelector('html');
+const displayTempo = document.querySelector('#timer');
+
+const tipoImagem = document.querySelector('img.app__image');
+const fraseTitle = document.querySelector('.app__title');
+
+const initBtn = document.getElementById('start-pause');
+
 const focoBtn = document.querySelector('.app__card-button--foco');
 const curtoBtn = document.querySelector('.app__card-button--curto');
 const longoBtn = document.querySelector('.app__card-button--longo');
 
+const duracaoFoco = 1500; 
+const duracaoDescansoCurto = 300; 
+const duracaoDescansoLongo = 900; 
+
+
 focoBtn.addEventListener('click', () => {
-    html.setAttribute('data-contexto', 'foco')
+    alterarContexto('foco');
 });
 
 curtoBtn.addEventListener('click', () => {
-    html.setAttribute('data-contexto', 'descanso-curto')
+    alterarContexto('descanso-curto');
 });
 
 longoBtn.addEventListener('click', () => {
-    html.setAttribute('data-contexto', 'descanso-longo')
+    alterarContexto('descanso-longo');
+
 });
+
+function alterarContexto(contexto){
+    html.setAttribute('data-contexto', contexto);
+    tipoImagem.setAttribute('src', `/imagens/${contexto}.png`);
+}
